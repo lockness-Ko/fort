3aac6497a1754e247ee6528718a811b1e58d51c6
lockness-Ko <videolk9373@gmail.com>
2022-11-06 19:51:56 +1100

    (files) reorganize stuff for docker

---
+++ app/src/lib/components/Header.svelte
@@ -0,0 +1,34 @@
+<header>
+  <a href="/">home</a>
+
+  <nav>
+    <ul>
+      <li>
+        <a href="/upload">upload</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<style>
+  header {
+    padding: 1rem;
+    background: #2222222b;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+
+  ul {
+    margin: 0;
+    list-style-type: none;
+    display: flex;
+    gap: 1rem;
+  }
+
+  a {
+    text-decoration: none;
+    color: #eee;
+  }
+</style>
+
---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+  import File from '$lib/components/File.svelte'
+
+  let files_promise = fetch('/api/files').then((x) => x.json());
+</script>
+
+<h1>hello, town</h1>
+
+<div class="files">
+  <ul>
+  {#await files_promise then files}
+    {#each files as file}
+    <li>
+      <File filename={file["name"]} date={file["date"]}/>
+    </li>
+    {/each}
+  {/await}
+  </ul>
+</div>
+
+<style>
+</style>
