ddc5bac636fa95b6e3815af3609db968e8bc9d65
lockness-Ko <videolk9373@gmail.com>
2022-11-07 10:26:13 +1100

    (files) add more file display options

--- app/src/routes/files/[slug]/+page.svelte
+++ app/src/routes/files/[slug]/+page.svelte
@@ -5,6 +5,8 @@
   let ext = slug.toLowerCase();
   
   let thumb = `/thumb/?${slug.split('.')[0]}.webp`;
+  
+  let script = `<script>window.onload = () => { fetch('/download/?${slug}').then(x => x.text()).then(y => document.querySelector('#txt_container').textContent = y);}<\/script>`;
 </script>
 
 <head>
@@ -16,12 +18,15 @@
   <h1>{slug}</h1> <span><a href="/download/?{slug}" download>download</a></span>
 </div>
 
-{#if ext.endsWith(".jpg") || ext.endsWith(".png") || ext.endsWith(".webm") || ext.endsWith(".gif") || ext.endsWith(".apng") }
+{#if ext.endsWith(".jpg") || ext.endsWith(".png") || ext.endsWith(".webp") || ext.endsWith(".gif") || ext.endsWith(".apng") }
   <img src="/download/?{slug}"/>
 {:else if ext.endsWith(".mp4") || ext.endsWith(".webm") || ext.endsWith(".mov") }
   <video src="/download/?{slug}" controls/>
 {:else if ext.endsWith(".mp3") || ext.endsWith(".wav") || ext.endsWith(".flac") }
   <audio src="/download/?{slug}" controls/>
+{:else if ext.endsWith(".txt") || ext.endsWith(".c") || ext.endsWith(".cc") || ext.endsWith(".rs") || ext.endsWith(".py") || ext.endsWith(".js") || ext.endsWith(".html") || ext.endsWith(".css") || ext.endsWith(".sh") || ext.endsWith(".md")}
+  <pre id="txt_container"></pre>
+  {@html script}
 {:else}
   <h3>Oh no! It looks like this file can't be previewed. <a href="/download/?{slug}" download>Click <u>here</u> to download instead.</a></h3>
 {/if}
