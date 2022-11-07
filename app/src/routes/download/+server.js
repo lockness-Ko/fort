6dc94d2abea7e997480fb7ffe63294940839de5d
lockness-Ko <videolk9373@gmail.com>
2022-11-07 13:51:05 +1100

    (fix) url encoding in /download and /thumb

--- app/src/routes/download/+server.js
+++ app/src/routes/download/+server.js
@@ -2,6 +2,6 @@ import { readFileSync } from 'fs';
 import { STORAGE_PATH } from "$env/static/private";
 
 export async function GET({ url }) {
-  let filename = url.search.replaceAll('?', '');
+  let filename = decodeURI(url.search.replaceAll('?', ''));
   return new Response(readFileSync(`${STORAGE_PATH}/static/download/${filename}`));
 }
