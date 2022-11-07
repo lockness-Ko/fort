60be77f6f65589cf826b4fec9c0f297fd0c6a602
lockness-Ko <videolk9373@gmail.com>
2022-11-07 17:55:09 +1100

    (fix) out of order dates in forecast

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -5,13 +5,13 @@
   let weather_promise = fetch('/api/weather').then((x) => x.json());
   
   let days = {
-    1: "mon",
-    2: "tue",
-    3: "wed",
-    4: "thu",
-    5: "fri",
-    6: "sat",
-    7: "sun",
+    0: "mon",
+    1: "tue",
+    2: "wed",
+    3: "thu",
+    4: "fri",
+    5: "sat",
+    6: "sun",
   };
   function toDayName(day) {
     return days[day]
