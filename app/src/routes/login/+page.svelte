7a71ef564648748fa2907a2b9b607b468fd4205e
lockness-Ko <videolk9373@gmail.com>
2022-11-13 10:03:00 +1100

    (update) login page styles

--- app/src/routes/login/+page.svelte
+++ app/src/routes/login/+page.svelte
@@ -1,5 +1,27 @@
 <form method="POST" action="/api/login">
-  <input type="text" name="user" placeholder="user."/>
-  <input type="password" name="pass" placeholder="password."/>
-  <input type="submit">
+  <div class="form-inputs">
+    <input type="text" name="user" placeholder="user."/>
+    <input type="password" name="pass" placeholder="password."/>
+    <input type="submit" value="login">
+  </div>
 </form>
+
+<style>
+  .form-inputs {
+    display: flex;
+    flex-direction: column;
+  }
+  
+  input {
+    margin: auto;
+    padding: 10px;
+    margin-bottom: 5px;
+    
+    font-size: inherit;
+  }
+  
+  input[type=text], input[type=password] {
+    border: solid 1px #aaa;
+    border-radius: 2px;
+  }
+</style>
