fcc4b59489c06e9a52e33666cef2101831457b92
lockness-Ko <videolk9373@gmail.com>
2022-11-13 11:42:10 +1100

    (fix) create share path

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ HASH=$(node ./run.js admin)
 redis-cli -h redis SET admin $HASH
 
 # Setup storage
-mkdir -p $STORAGE_PATH/static/download $STORAGE_PATH/static/thumb
+mkdir -p $STORAGE_PATH/static/download $STORAGE_PATH/static/thumb $STORAGE_PATH/static/share
 
 # Run web app
 npm run preview &
