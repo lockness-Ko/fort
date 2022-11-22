e9226e1a907e9c184f86ca1a6216f5755ba46dbb
lockness-Ko <videolk9373@gmail.com>
2022-11-22 19:55:42 +1100

    (fix) deletion user error

--- app/src/routes/delete/+server.js
+++ app/src/routes/delete/+server.js
@@ -4,12 +4,16 @@ import { redirect } from '@sveltejs/kit';
 import url from 'url';
 
 export async function GET ({ request }) {
+  let cookies = cookie.parse(request.headers.get('cookie'));
+  let sid = cookies['sid'];
+  let user = jwt.decode(sid)['user'];
+  
   let filename = decodeURI(new url.URL(request.url).search.split('?')[1]);
   if (filename.includes('..')) {
     return new Response(400);
   }
 
-  await unlink(`${STORAGE_PATH}/static/download${filename}`);
+  await unlink(`${STORAGE_PATH}/static/download/${user}${filename}`);
   
   throw redirect(307, "/")
 }
