cd756a17bf8ee02e833496b70feb69a993fa88b4
lockness-Ko <videolk9373@gmail.com>
2022-12-05 17:40:47 +1100

    (update) finalize todo page

--- app/src/lib/components/Todo.svelte
+++ app/src/lib/components/Todo.svelte
@@ -2,11 +2,29 @@
   import { createEventDispatcher } from 'svelte';
   export let value;
   export let done;
+  export let newTodo;
 
   const dispatch = createEventDispatcher();
   const change = () => dispatch('change', value);
 
+  if (newTodo) {
+    done = false;
+    value = "";
+  }
 </script>
 
-<input type="checkbox" on:change={change} checked={done} name={value}/>
-<label for={value}>{value}</label>
+{#if newTodo}
+  <label for={value}><input type="text" on:change={change} placeholder="new todo.." /></label>
+{:else}
+  <input type="checkbox" on:change={change} checked={done} name={value}/>
+  <label for={value}>{value}</label>
+{/if}
+
+<style>
+  input[type=text] {
+    color: #eee;
+    font-size: 17.5px;
+    margin-left: 25px;
+    padding: 0px;
+  }
+</style>
--- app/src/routes/todo/+page.svelte
+++ app/src/routes/todo/+page.svelte
@@ -18,16 +18,36 @@
       body: JSON.stringify(todos)
     })
   }
+  
+  function newTodoTicked(e) {
+    let newTodo = e.explicitOriginalTarget;
+    
+    todos[newTodo.value] = false;
+    newTodo.value = "";
+
+    fetch(`/api/todo`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(todos)
+    })
+  }
 </script>
 
 <h1>🗒️</h1>
 
 <ul>
   {#each Object.keys(todos) as todo}
-    <li>
-      <Todo on:change={todoTicked} value={todo} done={todos[todo]}/>
-    </li>
+    {#if !todos[todo]}
+      <li>
+        <Todo on:change={todoTicked} value={todo} done={todos[todo]}/>
+      </li>
+    {/if}
   {/each}
+  <li>
+    <Todo newTodo={true} on:change={newTodoTicked}/>
+  </li>
 </ul>
 
 <style>
